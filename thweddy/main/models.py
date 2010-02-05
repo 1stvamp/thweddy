772bf2217b9d88fac59b4d79caa2e408956661dc
Wesley Mason <wes [at] 1stvamp.org>
2010-02-05 16:57:51 +0000

    Base models

--- thweddy/main/models.py
+++ thweddy/main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+
+class TwitterUser(models.Model):
+    username = models.CharField(max_length=140)
+
+
+class Tweet(models.Model):
+    tweet_id = models.CharField(max_length=255)
+    sort = models.IntegerField()
+
+
+class Thread(models.Model):
+    user = models.ForeignKey('TwitterUser')
+    tweets = models.ManyToManyField('Tweet')
+    hash = models.CharField(max_length=255)
+
+
+admin.site.register(TwitterUser)
+admin.site.register(Tweet)
+admin.site.register(Thread)
